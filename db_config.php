531852ceaccb52b23b8c9e8f03e3778adf40ef8c
YuraEnder <yuraender@yandex.ru>
2024-12-19 19:17:00 +0300

    Update db server

--- db_config.php
+++ db_config.php
@@ -2,5 +2,5 @@
     define('DB_USER', "u0753756_nkz_usr"); //логин БД
     define('DB_PASSWORD', "xW3wE3dZ4whB2dV3"); // пароль БД
     define('DB_DATABASE', "u0753756_nkz_db"); // база данных
-    define('DB_SERVER', "127.0.0.1"); // сервер 'localhost'
+    define('DB_SERVER', "cika-da.ru"); // сервер
 ?>
